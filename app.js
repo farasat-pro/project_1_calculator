4dfadfd13623b8dc89b1b146efb9acc1b87eb8c3
farasat-pro <farasatgg@gmail.com>
2026-06-06 12:52:50 +0500

    code uploaded

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("JS connected succesfully!")
+
+function check_operation(opp) {
+    switch (opp) {
+        case 1:
+            console.log("pluse")
+            break;
+        case 2:
+            console.log("minus")
+            break;
+        case 3:
+            console.log("divide")
+            break;
+        case 4:
+            console.log("multiply")
+            break;
+        case 5:
+            console.log("mode")
+            break;    
+        default:
+            console.log("error in check_operator")                
+    }
+
+}
